7a00c4dce5730cccb46ddd84156ad7113c5f3d97
Donald Wilcox <dowilcox@umflint.edu>
2017-02-02 08:27:15 -0500

    New redis store class.

---
+++ src/RedisStore.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace UMFlint\Cache\RedisStore;
+
+use Illuminate\Cache\RedisStore as RedisStoreBase;
+
+class RedisStore extends RedisStoreBase
+{
+    /**
+     * Name of the set that holds all the keys.
+     *
+     * @var string
+     */
+    protected $setName = 'key.store';
+
+    /**
+     * Add a cache key to the set.
+     *
+     * @author Donald Wilcox <dowilcox@umflint.edu>
+     * @param $key
+     */
+    public function addKey($key)
+    {
+        $this->connection()->command('SADD', [
+            $this->prefix . $this->setName,
+            $this->prefix . $key,
+        ]);
+    }
+
+    /**
+     * Remove a cache key from the set.
+     *
+     * @author Donald Wilcox <dowilcox@umflint.edu>
+     * @param $key
+     */
+    public function removeKey($key)
+    {
+        $this->connection()->command('SREM', [
+            $this->prefix . $this->setName,
+            $this->prefix . $key,
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function put($key, $value, $minutes)
+    {
+        parent::put($key, $value, $minutes);
+
+        $this->addKey($key);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function increment($key, $value = 1)
+    {
+        $this->addKey($key);
+
+        return parent::increment($key, $value);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function decrement($key, $value = 1)
+    {
+        $this->addKey($key);
+
+        return parent::decrement($key, $value);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function forever($key, $value)
+    {
+        parent::forever($key, $value);
+
+        $this->addKey($key);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function forget($key)
+    {
+        $this->removeKey($key);
+
+        return parent::forget($key);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function flush()
+    {
+        // Get the keys from the set.
+        $keys = $this->connection()->command('SMEMBERS', [
+            $this->prefix . $this->setName,
+        ]);
+
+        // Delete all the keys.
+        $this->connection()->command('DEL', $keys);
+
+        // Clear the set.
+        $this->connection()->command('DEL', $this->prefix . $this->setName);
+    }
+}
